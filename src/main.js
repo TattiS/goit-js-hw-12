ebc3489003fdc1dccffb78c474388d80b030c81f
Tetyana Solyanyk <tanyas81@gmail.com>
2025-01-21 02:21:05 +0200

    move main js with some changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
-import { getData } from './pixabay-api.js';
-import { createGalleryCardTemplate } from './render-functions.js';
-import { showErrMsg } from './render-functions.js';
+import { getData } from './js/pixabay-api.js';
+import { createGalleryCardTemplate } from './js/render-functions.js';
+import { showErrMsg } from './js/render-functions.js';
 
 const searchPath = 'https://pixabay.com/api/';
 const searchOptions = {
@@ -26,11 +26,13 @@ const onSearchFormSubmit = event => {
   event.preventDefault();
   gallerycards = '';
   galleryEl.innerHTML = '';
-  if (!searchStrInput.value.trim()) {
+  const searchStr = event.currentTarget.elements.searchStr.value.trim();
+
+  if (!searchStr) {
     showErrMsg('Enter a string to start a new search, please.');
     return;
   }
-  searchOptions.q = searchStrInput.value.trim();
+  searchOptions.q = searchStr;
   searchFormEl.reset();
   toggleLoader();
   getData(searchPath, searchOptions)
@@ -42,9 +44,9 @@ const onSearchFormSubmit = event => {
         );
         return;
       }
-      data.hits.map(picInfo => {
-        gallerycards += createGalleryCardTemplate(picInfo);
-      });
+      gallerycards = data.hits
+        .map(picInfo => createGalleryCardTemplate(picInfo))
+        .join('');
       toggleLoader();
       galleryEl.innerHTML = gallerycards;
       modalWindow.refresh();
